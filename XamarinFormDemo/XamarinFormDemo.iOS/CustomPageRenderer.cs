d81797a3051ebdbd2837f8bbc254238cc097a57d
Bence Lenart <bence960206@gmail.com>
2020-03-13 09:46:09 +0100

    CustomPageRenderer iOS

---
+++ XamarinFormDemo/XamarinFormDemo.iOS/CustomPageRenderer.cs
@@ -0,0 +1,28 @@
+﻿using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using XamarinFormDemo.iOS;
+
+[assembly: ExportRenderer(typeof(Page), typeof(CustomPageRenderer))]
+namespace XamarinFormDemo.iOS
+{
+    public class CustomPageRenderer : PageRenderer, IUIGestureRecognizerDelegate
+    {
+        [Preserve(Conditional = true)]
+        public CustomPageRenderer()
+        {
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+
+            if (NavigationController != null)
+            {
+                NavigationController.InteractivePopGestureRecognizer.Delegate = this;
+                NavigationController.InteractivePopGestureRecognizer.Enabled = true;
+            }
+        }
+    }
+}
